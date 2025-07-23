8350b8dbb02d31e74ee04b9404cb0f049d7b3d31
Shangesh S <shangesh2006@gmail.com>
2025-07-23 20:24:07 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -44,7 +44,7 @@ app.use(
     origin:
       process.env.NODE_ENV === "production"
         ? ["https://realtime-chat-coral-eight.vercel.app"]
-        : ["http://localhost:3000", "http://localhost:3001"],
+        : ["http://localhost:3000", "http://localhost:3001", "https://realtime-chat-coral-eight.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
